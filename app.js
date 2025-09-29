326819ba83a4fb283308eec7214e52dfe2c17430
Your Name <you@example.com>
2025-09-29 18:03:16 +0300

    First update

---
+++ app.js
@@ -0,0 +1,76 @@
+const modal = document.querySelector('#modal')
+const content = document.querySelector('#content')
+const backdrop = document.querySelector('#backdrop')
+
+content.addEventListener('click', openCard)
+backdrop.addEventListener('click', closeModal)
+
+const technologies = [
+    {
+     title:'HTML',
+     description:'HTML Text',
+     type:'html',
+     done:true
+    },
+     {
+     title:'CSS',
+     description:'CSS Text',
+     type:'css',
+     done:true
+    },
+     {
+     title:'JavaScript',
+     description:'Javascript Text',
+     type:'js',
+     done:false
+    },
+     {
+     title:'Git',
+     description:'Git Text',
+     type:'git',
+     done:false
+    },
+     {
+     title:'React',
+     description:'React Text',
+     type:'react',
+     done:false
+    }
+]
+
+function openCard(){
+    modal.classList.add('open')
+}
+
+function closeModal(){
+    modal.classList.remove('open')
+}
+
+function init(){
+    if(technologies.length === 0){
+        content.innerHTML = '<p class="empty">Технологий пока нет</p>'
+    } else{
+        let html = ''
+        for (let i = 0 ; i < technologies.length; i++){
+        const tech = technologies[i]
+        html += toCard(tech)
+            }
+        content.innerHTML = html
+    }
+}
+
+function toCard(tech){
+    let doneClass = ''
+
+    if(tech.done){
+        doneClass = 'done';
+    } 
+
+    return `
+        <div class="card ${doneClass}">
+            <h3>${tech.title}</h3>
+        </div>
+        `
+}
+
+init()
